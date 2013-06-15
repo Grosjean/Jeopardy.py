6bfbddc5f0b95830cc544f2831df3bedf9323611
Alex Grosjean <LinxuSx@gmail.com>
2013-06-15 15:09:22 -0500

    Version 2

--- Jeopardy.py
+++ Jeopardy.py
@@ -1,5 +1,5 @@
 import sqlite3
-import texttable
+import os, os.path, time, sys, random
 
 """
 CATEGORY
@@ -26,44 +26,70 @@ cursor = connection.cursor()
 
 def build_query_results(query):
 	"""builds the query results based on input and returns the list"""
+	
 	cursor.execute(query)
 	return cursor.fetchall()
 
-def get_random_categories():
-	"""build a query of random category names from database and prints them out"""
-	query = "SELECT id, name FROM category ORDER BY RANDOM() LIMIT %i" % 5
+def get_random_categories(num):
+	"""build a query of random category names from database and prints them out
+	takes the number of categories to query"""
+	
+	query = "SELECT id, name FROM category ORDER BY RANDOM() LIMIT %i" % num
 	randCats = build_query_results(query)
 	return randCats
 
 def print_categories(randCats):
+	"""Prints out the random categories"""
 	print "\n"
 	for (counter,cat) in enumerate(randCats):
-		print "%s : %s (%s)" % (counter+1,cat[1],cat[0])
+		print "\t%s : %s" % (counter+1,cat[1])
+	print "\t" + "-" * 25
+	print "\t0 : Enter 0 to exit"
 	print "\n"
 	
-def get_questions_from_category(catNum):	
+def get_questions_from_category(catNum):
+	"""Querieis the specified categoryto grab question and answer"""
 	query = "SELECT text, answer, value, isDD FROM clue WHERE category=%s LIMIT %i" % (catNum, 10)
 	questions = build_query_results(query)
 	return questions
+
+def print_category_questions(catQuestions):
+	"""prints out the question and answers based on catQuestions query"""
+	for cat in catQuestions:
+		print "QUESTION: %s " % cat[0]
+		print "ANSWER: %s " % cat[1]
+		print "VALUE: %s " % cat[2]
+		if cat[3] == "1":
+			print "Daily Double!!"
+		print "=" * 20
 	
-randCats = get_random_categories()
-print_categories(randCats)
 
-category = int(raw_input("Which category: ")) -1
+randCats = get_random_categories(6) #gets 6 random categories and stores as randCats list
 
-catQuestions = get_questions_from_category(randCats[category][0])
+continueGame = True
 
-for cat in catQuestions:
-	print "QUESTION: %s " % cat[0]
-	print "ANSWER: %s " % cat[1]
-	print "VALUE: %s " % cat[2]
-	if cat[3] == "1":
-		print "Daily Double!!" % cat[3]
-	print "=" * 20
+while continueGame:
+
+	print_categories(randCats) #prints out the new rand cats
+	
+	try:
+		category = int(raw_input("Which category: ")) - 1
+	except ValueError:
+		print "\nIncorrect Value!"
+		break
+	
+	if category == -1:
+		continueGame = False
+	else:
+		catQuestions = get_questions_from_category(randCats[category][0]) #sends the select category to print out a list of questions
+		print_category_questions(catQuestions)
+	
+		time.sleep(2)
+	
+else:
+	print "\n\nThanks for Playing!!\n"
 
 connection.close()
-
-#59 founding fathers
 
 """
 results = build_query_results("SELECT text, answer, value FROM clue LIMIT 10")
